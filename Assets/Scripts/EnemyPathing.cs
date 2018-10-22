efb19c08619d420bad1b2ff7ee8c0c432e3f719d
Marian Stanciulica <marian.stanciulica@gmail.com>
2018-10-22 18:29:56 +0300

    Setup Enemy Path

--- Assets/Scripts/EnemyPathing.cs
+++ Assets/Scripts/EnemyPathing.cs
@@ -4,17 +4,25 @@ using UnityEngine;
 
 public class EnemyPathing : MonoBehaviour {
 
-    [SerializeField] List<Transform> waypoints;
+    [SerializeField] WaveConfig waveConfig;
+    List<Transform> waypoints;
     [SerializeField] float moveSpeed = 2f;
     int waypointIndex = 0;
 
 	// Use this for initialization
 	void Start () {
+        waypoints = waveConfig.GetWaypoints();
         transform.position = waypoints[waypointIndex].transform.position;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
         if (waypointIndex <= waypoints.Count - 1)
         {
             var targetPosition = waypoints[waypointIndex].transform.position;
@@ -30,5 +38,5 @@ public class EnemyPathing : MonoBehaviour {
         {
             Destroy(gameObject);
         }
-	}
+    }
 }
